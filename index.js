0dcd102ae2c441c1076ae824e520337186375665
Tatiana Sherrill <anilorac_sol@yahoo.com>
2018-07-22 21:01:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -27,8 +27,13 @@ function destructivelyRemoveFirstKitten(){
 }
 
 function appendKitten(name){
-  var kittens = ["Milo", "Otis", "Garfield"]
+  //var kittens = ["Milo", "Otis", "Garfield"]
   var newArray = kittens.slice()
   newArray.push(name)
   console.log(newArray)
+}
+
+function removeLastKitten(){
+  kittens = ["Milo", "Otis", "Garfield"]
+  
 }
